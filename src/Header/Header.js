b62e33625207d267895c25b36eaaf2ad1047141a
unknown <mitch1080@hotmail.co.uk>
2023-02-21 20:47:24 +0000

    Change site name

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -4,7 +4,7 @@ import React from "react";
 const Header = () => {
   return (
     <React.Fragment>
-      <div className={style.header}>Mit.ch</div>
+      <div className={style.header}>Mitlan</div>
       <div className={style.aboutme}>
         <h2>About Me</h2>
         <p class="description">
